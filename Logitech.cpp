a5ddcd75cdabf21375d6f2cca0392824f83385d1
jimmyd <dom.jimmy@gmail.com>
2012-12-15 14:21:54 +0100

    Code optimalized. Crashes fixed.

--- Logitech.cpp
+++ Logitech.cpp
@@ -1,16 +1,23 @@
+//-----------------------------------------------------------------
+// Logitech File
+// C++ Source - Logitech.cpp - version 2012 v1.0
+//-----------------------------------------------------------------
 
-// Logitech.cpp : implementation file
-//
-
+//-----------------------------------------------------------------
+// Include Files
+//-----------------------------------------------------------------
 #include "stdafx.h"
 #include "Logitech.h"
 
+//-----------------------------------------------------------------
+// Logitech methods
+//-----------------------------------------------------------------
+
 Logitech * Logitech::object;
 
-Logitech::Logitech()
+Logitech::Logitech():	stopthread(false), firstTime(true), position(0), duration(0)
 {
 	object = this;
-	stopthread = false;
 }
 
 Logitech::~Logitech()
@@ -26,87 +33,34 @@ BOOL Logitech::OnInitDialog()
 
 	if (hRes != S_OK)
 	{
-		// Something went wrong, when connecting to the LCD Manager software. Deal with it...
-		PostQuitMessage(0);
 		return FALSE;
 	}
 
-	timerThread = thread(&Logitech::startThread);
-
 	if(m_lcd.IsDeviceAvailable(LG_COLOR))
 	{
-		InitLCDObjectsColor();
+		m_lcd.ModifyDisplay(LG_COLOR);
+		logo = m_lcd.AddText(LG_STATIC_TEXT, LG_BIG, DT_CENTER, LGLCD_BW_BMP_WIDTH);
+		m_lcd.SetOrigin(logo, 0, 50);
+		m_lcd.SetText(logo, _T("MusicBee"));
+		m_lcd.Update();
 	}
 
 	else if(m_lcd.IsDeviceAvailable(LG_MONOCHROME))
 	{
-		InitLCDObjectsMonochrome();
+		m_lcd.ModifyDisplay(LG_MONOCHROME);
+		logo = m_lcd.AddText(LG_STATIC_TEXT, LG_BIG, DT_CENTER, LGLCD_BW_BMP_WIDTH);
+		m_lcd.SetOrigin(logo, 0, 5);
+		m_lcd.SetText(logo, _T("MusicBee"));
+		m_lcd.Update();
 	}
+
+	timerThread = thread(&Logitech::startThread);
 
 	return TRUE;  // return TRUE  unless you set the focus to a control
 }
 
-void Logitech::startThread()
+VOID Logitech::createMonochrome()
 {
-	while(!object->stopthread)
-	{
-		this_thread::sleep_for( chrono::milliseconds(500) );
-		if(object->state == 3)
-		{
-			this_thread::sleep_for( chrono::milliseconds(500) );
-			object->position++;
-
-			object->m_lcd.SetProgressBarPosition(object->progressbar, static_cast<FLOAT>(((float)object->position / object->duration)*100));
-			object->m_lcd.SetText(object->time, object->getPositionString().c_str());
-		}
-
-		else if(object->state == 7)
-		{
-			object->position = 0;
-			object->m_lcd.SetProgressBarPosition(object->progressbar, 0);
-			object->m_lcd.SetText(object->time, object->getPositionString().c_str());
-		}
-
-		object->m_lcd.Update();
-	}
-}
-
-VOID Logitech::InitLCDObjectsMonochrome()
-{
-	m_lcd.ModifyDisplay(LG_MONOCHROME);
-
-	m_lcd.RemovePage(0);
-	m_lcd.AddNewPage();
-	m_lcd.ShowPage(0);
-
-	logo = m_lcd.AddText(LG_STATIC_TEXT, LG_BIG, DT_CENTER, LGLCD_BW_BMP_WIDTH);
-	m_lcd.SetOrigin(logo, 0, 5);
-	m_lcd.SetText(logo, _T("MusicBee"));
-	m_lcd.Update();
-}
-
-VOID Logitech::InitLCDObjectsColor()
-{
-	m_lcd.ModifyDisplay(LG_COLOR);
-
-	m_lcd.RemovePage(0);
-	m_lcd.AddNewPage();
-	m_lcd.ShowPage(0);
-
-	logo = m_lcd.AddText(LG_STATIC_TEXT, LG_BIG, DT_CENTER, LGLCD_BW_BMP_WIDTH);
-	m_lcd.SetOrigin(logo, 0, 50);
-	m_lcd.SetText(logo, _T("MusicBee"));
-	m_lcd.Update();
-}
-
-void Logitech::changeArtistTitle(wstring artistStr, wstring titleStr, wstring duration, int position)
-{
-	this->artistString = artistStr;
-	this->titleString = titleStr;
-	this->durationString = duration;
-	this->position = position;
-	this->duration = getDuration(duration);
-
 	m_lcd.RemovePage(0);
 	m_lcd.AddNewPage();
 	m_lcd.ShowPage(0);
@@ -117,18 +71,122 @@ void Logitech::changeArtistTitle(wstring artistStr, wstring titleStr, wstring du
 		logo = 0;
 	}
 
+	artist = m_lcd.AddText(LG_SCROLLING_TEXT, LG_MEDIUM, DT_CENTER, LGLCD_BW_BMP_WIDTH);
+	m_lcd.SetOrigin(artist, 0, 0);
+
+	title = m_lcd.AddText(LG_SCROLLING_TEXT, LG_MEDIUM, DT_CENTER, LGLCD_BW_BMP_WIDTH);
+	m_lcd.SetOrigin(title, 0, 13);
+
+	progressbar = m_lcd.AddProgressBar(LG_DOT_CURSOR);
+	m_lcd.SetProgressBarSize(progressbar, 136, 5);
+	m_lcd.SetOrigin(progressbar, 12, 38);
+
+	time = m_lcd.AddText(LG_STATIC_TEXT, LG_SMALL, DT_LEFT, 80);
+	m_lcd.SetOrigin(time, 12, 29);
+
+	/*	playIcon = static_cast<HICON>(LoadImage(AfxGetInstanceHandle(), MAKEINTRESOURCE(IDB_PNG1), IMAGE_ICON, 16, 16, LR_MONOCHROME));
+	playIconHandle = m_lcd.AddIcon(playIcon, 16, 16);
+	m_lcd.SetOrigin(playIconHandle, 2, 29);*/
+
+	firstTime = false;
+	changeArtistTitle(this->artistString, this->albumString, this->titleString, this->durationString, this->position);
+}
+
+VOID Logitech::createColor()
+{
+	m_lcd.RemovePage(0);
+	m_lcd.AddNewPage();
+	m_lcd.ShowPage(0);
+
+	if (logo != 0)
+	{
+		delete logo;
+		logo = 0;
+	}
+
+	artist = m_lcd.AddText(LG_SCROLLING_TEXT, LG_MEDIUM, DT_LEFT, 310);
+	m_lcd.SetOrigin(artist, 5, 5);
+
+	album = m_lcd.AddText(LG_SCROLLING_TEXT, LG_MEDIUM, DT_LEFT, 310);
+	m_lcd.SetOrigin(album, 5, 30);
+
+	title = m_lcd.AddText(LG_SCROLLING_TEXT, LG_MEDIUM, DT_LEFT, 310);
+	m_lcd.SetOrigin(title, 5, 55);
+
+	time = m_lcd.AddText(LG_STATIC_TEXT, LG_SMALL, DT_LEFT, 80);
+	m_lcd.SetOrigin(time, 5, 80);
+
+	time1 = m_lcd.AddText(LG_STATIC_TEXT, LG_SMALL, DT_LEFT, 80);
+	m_lcd.SetOrigin(time1, 275, 80);
+
+	progressbar = m_lcd.AddProgressBar(LG_DOT_CURSOR);//320×240 pixel color screen
+	m_lcd.SetProgressBarSize(progressbar, 310, 15);
+	m_lcd.SetOrigin(progressbar, 5, 100);
+
+	/*playIcon = static_cast<HICON>(LoadImage(AfxGetInstanceHandle(), MAKEINTRESOURCE(IDB_PNG1), IMAGE_ICON, 16, 16, LR_COLOR));
+	playIconHandle = m_lcd.AddIcon(playIcon, 16, 16);
+	m_lcd.SetOrigin(playIconHandle, 5, 29);*/
+
+	firstTime = false;
+	changeArtistTitle(this->artistString, this->albumString, this->titleString, this->durationString, this->position);
+}
+
+void Logitech::startThread()
+{
+	while(!object->stopthread)
+	{
+		this_thread::sleep_for( chrono::milliseconds(500) );
+
+		if(!object->stopthread && object->progressbar != NULL)
+		{
+			if(object->state == 3)
+			{
+				this_thread::sleep_for( chrono::milliseconds(500) );
+				object->position++;
+
+				object->m_lcd.SetProgressBarPosition(object->progressbar, static_cast<FLOAT>(((float)object->position / object->duration)*100));
+				object->m_lcd.SetText(object->time, object->getPositionString().c_str());
+			}
+
+			else if(object->state == 7)
+			{
+				object->position = 0;
+				object->m_lcd.SetProgressBarPosition(object->progressbar, 0);
+				object->m_lcd.SetText(object->time, object->getPositionString().c_str());
+			}
+
+			object->m_lcd.Update();
+		}
+	}
+}
+
+void Logitech::changeArtistTitle(wstring artistStr, wstring albumStr, wstring titleStr, wstring duration, int position)
+{
+	this->artistString = artistStr;
+	this->albumString = albumStr;
+	this->titleString = titleStr;
+	this->durationString = duration;
+	this->position = position;
+	this->duration = getDuration(duration);
+
+	if(!firstTime)
+	{
 	if(m_lcd.IsDeviceAvailable(LG_COLOR))
 	{
-		artist = m_lcd.AddText(LG_SCROLLING_TEXT, LG_MEDIUM, DT_LEFT, 320);
-		m_lcd.SetOrigin(artist, 5, 5);
+		//artist = m_lcd.AddText(LG_SCROLLING_TEXT, LG_MEDIUM, DT_LEFT, 310);
+		//m_lcd.SetOrigin(artist, 5, 5);
 		m_lcd.SetText(artist, artistStr.c_str());
 
-		title = m_lcd.AddText(LG_SCROLLING_TEXT, LG_MEDIUM, DT_LEFT, 320);
-		m_lcd.SetOrigin(title, 5, 30);
+		//album = m_lcd.AddText(LG_SCROLLING_TEXT, LG_MEDIUM, DT_LEFT, 310);
+		//m_lcd.SetOrigin(album, 5, 30);
+		m_lcd.SetText(album, albumStr.c_str());
+
+		//title = m_lcd.AddText(LG_SCROLLING_TEXT, LG_MEDIUM, DT_LEFT, 310);
+		//m_lcd.SetOrigin(title, 5, 55);
 		m_lcd.SetText(title, titleStr.c_str());
 
-		time = m_lcd.AddText(LG_STATIC_TEXT, LG_SMALL, DT_LEFT, 80);
-		m_lcd.SetOrigin(time, 5, 55);
+		//time = m_lcd.AddText(LG_STATIC_TEXT, LG_SMALL, DT_LEFT, 80);
+		//m_lcd.SetOrigin(time, 5, 80);
 		m_lcd.SetText(time, getPositionString().c_str());
 
 		string s( duration.begin(), duration.end() );
@@ -140,39 +198,39 @@ void Logitech::changeArtistTitle(wstring artistStr, wstring titleStr, wstring du
 
 		wstring ws( s.begin(), s.end() );
 		time1 = m_lcd.AddText(LG_STATIC_TEXT, LG_SMALL, DT_LEFT, 80);
-		m_lcd.SetOrigin(time1, 275, 55);
+		m_lcd.SetOrigin(time1, 275, 80);
 		m_lcd.SetText(time1, ws.c_str());
 		ws.clear();
-	
-		progressbar = m_lcd.AddProgressBar(LG_DOT_CURSOR);//320×240 pixel color screen
-		m_lcd.SetProgressBarSize(progressbar, 310, 15);
-		m_lcd.SetOrigin(progressbar, 5, 75);
-		m_lcd.SetProgressBarPosition(progressbar, static_cast<FLOAT>(((float)position / (float)this->duration)*100));
-		
-		/*playIcon = static_cast<HICON>(LoadImage(AfxGetInstanceHandle(), MAKEINTRESOURCE(IDB_PNG1), IMAGE_ICON, 16, 16, LR_COLOR));
-		playIconHandle = m_lcd.AddIcon(playIcon, 16, 16);
-		m_lcd.SetOrigin(playIconHandle, 5, 29);*/
+
+		//progressbar = m_lcd.AddProgressBar(LG_DOT_CURSOR);//320×240 pixel color screen
+		//m_lcd.SetProgressBarSize(progressbar, 310, 15);
+		//m_lcd.SetOrigin(progressbar, 5, 100);
+		////	m_lcd.SetProgressBarPosition(progressbar, static_cast<FLOAT>(((float)position / (float)this->duration)*100));
+
+		///*playIcon = static_cast<HICON>(LoadImage(AfxGetInstanceHandle(), MAKEINTRESOURCE(IDB_PNG1), IMAGE_ICON, 16, 16, LR_COLOR));
+		//playIconHandle = m_lcd.AddIcon(playIcon, 16, 16);
+		//m_lcd.SetOrigin(playIconHandle, 5, 29);*/
 
 		m_lcd.Update();
 	}
 
 	else if(m_lcd.IsDeviceAvailable(LG_MONOCHROME))
 	{
-		artist = m_lcd.AddText(LG_SCROLLING_TEXT, LG_MEDIUM, DT_CENTER, LGLCD_BW_BMP_WIDTH);
-		m_lcd.SetOrigin(artist, 0, 0);
+		//artist = m_lcd.AddText(LG_SCROLLING_TEXT, LG_MEDIUM, DT_CENTER, LGLCD_BW_BMP_WIDTH);
+		//m_lcd.SetOrigin(artist, 0, 0);
 		m_lcd.SetText(artist, artistStr.c_str());
 
-		title = m_lcd.AddText(LG_SCROLLING_TEXT, LG_MEDIUM, DT_CENTER, LGLCD_BW_BMP_WIDTH);
-		m_lcd.SetOrigin(title, 0, 13);
+		//title = m_lcd.AddText(LG_SCROLLING_TEXT, LG_MEDIUM, DT_CENTER, LGLCD_BW_BMP_WIDTH);
+		//m_lcd.SetOrigin(title, 0, 13);
 		m_lcd.SetText(title, titleStr.c_str());
 
-		progressbar = m_lcd.AddProgressBar(LG_DOT_CURSOR);
-		m_lcd.SetProgressBarSize(progressbar, 136, 5);
-		m_lcd.SetOrigin(progressbar, 12, 38);
-		m_lcd.SetProgressBarPosition(progressbar, static_cast<FLOAT>(((float)this->position / this->duration)*100));
+		//progressbar = m_lcd.AddProgressBar(LG_DOT_CURSOR);
+		//m_lcd.SetProgressBarSize(progressbar, 136, 5);
+		//m_lcd.SetOrigin(progressbar, 12, 38);
+		////	m_lcd.SetProgressBarPosition(progressbar, static_cast<FLOAT>(((float)this->position / this->duration)*100));
 
-		time = m_lcd.AddText(LG_STATIC_TEXT, LG_SMALL, DT_LEFT, 80);
-		m_lcd.SetOrigin(time, 12, 29);
+		//time = m_lcd.AddText(LG_STATIC_TEXT, LG_SMALL, DT_LEFT, 80);
+		//m_lcd.SetOrigin(time, 12, 29);
 		m_lcd.SetText(time, getPositionString().c_str());
 
 		string s( duration.begin(), duration.end() );
@@ -187,17 +245,19 @@ void Logitech::changeArtistTitle(wstring artistStr, wstring titleStr, wstring du
 		m_lcd.SetOrigin(time1, 125, 29);
 		m_lcd.SetText(time1, ws.c_str());
 		ws.clear();
-		
-		/*	playIcon = static_cast<HICON>(LoadImage(AfxGetInstanceHandle(), MAKEINTRESOURCE(IDB_PNG1), IMAGE_ICON, 16, 16, LR_MONOCHROME));
-		playIconHandle = m_lcd.AddIcon(playIcon, 16, 16);
-		m_lcd.SetOrigin(playIconHandle, 2, 29);*/
+
+		///*	playIcon = static_cast<HICON>(LoadImage(AfxGetInstanceHandle(), MAKEINTRESOURCE(IDB_PNG1), IMAGE_ICON, 16, 16, LR_MONOCHROME));
+		//playIconHandle = m_lcd.AddIcon(playIcon, 16, 16);
+		//m_lcd.SetOrigin(playIconHandle, 2, 29);*/
 
 		m_lcd.Update();
 	}
 
 	artistStr.clear();
+	albumStr.clear();
 	titleStr.clear();
 	duration.clear();
+	}
 }
 
 void Logitech::setPosition(int pos)
@@ -208,6 +268,7 @@ void Logitech::setPosition(int pos)
 		m_lcd.SetText(time1, getPositionString().c_str());
 	}
 }
+
 /*Undefined = 0,
 Loading = 1,
 Playing = 3,
@@ -216,6 +277,19 @@ Stopped = 7*/
 void Logitech::changeState(int state)
 {
 	this->state = state;
+
+	if(state == 3 && firstTime)
+	{
+		if(m_lcd.IsDeviceAvailable(LG_COLOR))
+		{
+			createColor();
+		}
+
+		else if(m_lcd.IsDeviceAvailable(LG_MONOCHROME))
+		{
+			createMonochrome();
+		}
+	}
 }
 
 int Logitech::getDuration(wstring duration)
@@ -250,75 +324,3 @@ wstring Logitech::getPositionString()
 
 	return wstring( time.begin(), time.end() );
 }
-
-/*VOID Logitech::CheckButtonPresses()
-{
-	if(m_lcd.IsDeviceAvailable(LG_MONOCHROME) && CheckbuttonPressesMonochrome())
-	{
-		InitLCDObjectsMonochrome();
-	}
-
-	else if(m_lcd.IsDeviceAvailable(LG_COLOR) && CheckbuttonPressesColor())
-	{
-		InitLCDObjectsColor();
-	}
-}
-
-bool Logitech::CheckbuttonPressesMonochrome()
-{
-	bool buttonPressed = false;
-
-	m_lcd.ModifyDisplay(LG_MONOCHROME);
-
-	if (m_lcd.ButtonTriggered(LG_BUTTON_4))
-	{
-
-	}
-
-	else if(m_lcd.ButtonTriggered(LG_BUTTON_1))
-	{
-
-	}
-
-	else if(m_lcd.ButtonTriggered(LG_BUTTON_2))
-	{
-
-	}
-
-	else if(m_lcd.ButtonTriggered(LG_BUTTON_3))
-	{
-
-	}
-
-	return buttonPressed;
-}
-
-bool Logitech::CheckbuttonPressesColor()
-{
-	m_lcd.ModifyDisplay(LG_COLOR);
-
-	bool buttonPressed = false;
-
-	if (m_lcd.ButtonReleased(LG_BUTTON_RIGHT))
-	{
-
-	}
-
-	if (m_lcd.ButtonReleased(LG_BUTTON_LEFT))
-	{
-
-	}
-
-	if (m_lcd.ButtonReleased(LG_BUTTON_DOWN))
-	{
-
-	}
-
-	if (m_lcd.ButtonReleased(LG_BUTTON_UP))
-	{
-
-	buttonPressed = true;
-	}
-
-	return buttonPressed;
-}*/
--- Logitech.h
+++ Logitech.h
@@ -1,34 +1,70 @@
+//-----------------------------------------------------------------
+// Logitech File
+// C++ Header - Logitech.h - version 2012 v1.0
+//-----------------------------------------------------------------
+
 #pragma once
 
+//-----------------------------------------------------------------
+// Include Files
+//-----------------------------------------------------------------
 
 #include "stdafx.h"
 #include "Src\EZ_LCD.h"
 #include "resource.h"
 #include "windows.h"
 #include <thread>
+#include <iostream>
+#include <fstream>
 
 using namespace std;
+
+//-----------------------------------------------------------------
+// Logitech Class
+//-----------------------------------------------------------------
 
 class Logitech
 {
 	// Construction
 public:
+	//---------------------------
+	// Constructor(s)
+	//---------------------------
 	Logitech();
+
+	//---------------------------
+	// Destructor
+	//---------------------------
 	~Logitech();
+
+	//---------------------------
+	// General Methods
+	//---------------------------
 	BOOL OnInitDialog();
 
-	void changeArtistTitle(wstring artist, wstring title, wstring time, int position);
+	void changeArtistTitle(wstring artistStr, wstring albumStr, wstring titleStr, wstring duration, int position);
 	void changeState(int state);
 	void setPosition(int);
 	static Logitech * object;
 	static void startThread();
+
 private:
+
+	int getDuration(wstring);
+	wstring getPositionString();
+	VOID createMonochrome();
+	VOID createColor();
+
+	// -------------------------
+	// Datamembers
+	// -------------------------
 	thread timerThread;
 	HICON m_hIcon;
 
 	CEzLcd m_lcd;
 	HANDLE logo;
 	HANDLE artist;
+	HANDLE album;
 	HANDLE title;
 	HANDLE progressbar;
 	HANDLE time;
@@ -37,19 +73,20 @@ private:
 	HICON playIcon;
 
 	bool stopthread;
+	bool firstTime;
 	wstring artistString;
+	wstring albumString;
 	wstring titleString;
 	wstring durationString;
 	int position;
 	int duration;
 	int state;
 
-	int getDuration(wstring);
-	wstring getPositionString();
-	VOID InitLCDObjectsMonochrome();
-	VOID InitLCDObjectsColor();
-
-	/*  VOID CheckButtonPresses();
-	bool CheckbuttonPressesMonochrome();
-	bool CheckbuttonPressesColor();*/
+	// -------------------------
+	// Disabling default copy constructor and default assignment operator.
+	// If you get a linker error from one of these functions, your class is internally trying to use them. This is
+	// an error in your class, these declarations are deliberately made without implementation because they should never be used.
+	// -------------------------
+	Logitech(const Logitech& t);
+	Logitech& operator=(const Logitech& t);
 };
